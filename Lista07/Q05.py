b26e2739be04d5343ed794853f8785bd50bd9e77
yScofield <ricardo0726@hotmail.com>
2023-06-05 18:18:05 -0300

    Lista07

--- Lista07/Q05.py
+++ Lista07/Q05.py
@@ -3,6 +3,8 @@ nome = nome.split()
 cont=0
 n=0
 for c in nome:
+    print (f'{c}', end="")
     n+= len(nome[cont])
     cont+=1
+print ('')
 print (f'Esse nome tem {n} letras.')
---
+++ Lista07/Q05b.py
@@ -0,0 +1,5 @@
+nome = input('Digite algo: ')
+nome = nome.replace(' ', '')
+
+print (f'O que você escreveu tem {len(nome)} letras.')
+print (nome)
